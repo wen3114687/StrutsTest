4da0b6493057a52edfd079c6a8112ff402d8b9b6
WBL <WBL@WBL>
2016-07-07 16:58:33 +0800

    2016-7-7 16:58:22  version 1.1

--- struts_1/src/com/wbl/action/LoginAction.java
+++ struts_1/src/com/wbl/action/LoginAction.java
@@ -1,4 +1,4 @@
-package com.wbl.login.action;
+package com.wbl.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
--- struts_1/src/com/wbl/interfaceManage/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author WBL
- *
- */
-package com.wbl.interfaceManage;
